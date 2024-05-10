0f0acd7fb7f7d2e661e30b72143d27610d910a23
mysoyul <vega2k_@naver.com>
2024-05-10 09:29:36 +0900

    CustomerRepositoryTest update

--- src/test/java/com/basic/myspringboot/customer/CustomerRepositoryTest.java
+++ src/test/java/com/basic/myspringboot/customer/CustomerRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +16,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 class CustomerRepositoryTest {
     @Autowired
     CustomerRepository customerRepository;
+
+    @Test
+    void notFound() {
+        Optional<Customer> notExistOptional = customerRepository.findByCustomerId("B001");
+        assertThat(notExistOptional).isEmpty();
+
+    }
 
     @Test @Rollback(value = false)
     void crud() {
@@ -29,5 +38,11 @@ class CustomerRepositoryTest {
 
         assertEquals("A001", addCustomer.getCustomerId());
 
+        Optional<Customer> optionalCustomer = customerRepository.findByCustomerId(addCustomer.getCustomerId());
+        assertThat(optionalCustomer).isNotEmpty();
+        Customer existCustomer = optionalCustomer.get();
+        //update - setter method 호출 dirty checking
+        existCustomer.setCustomerName("SpringBoot");
+        assertThat(existCustomer.getCustomerName()).isEqualTo("SpringBoot");
     }
 }
