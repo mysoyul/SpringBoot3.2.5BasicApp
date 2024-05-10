ec36ce5abcd8394439ed14d9af51eaa92bc1ace7
mysoyul <vega2k_@naver.com>
2024-05-10 17:27:20 +0900

    UserInfo repository add

--- src/main/java/com/basic/myspringboot/auth/userinfo/UserInfo.java
+++ src/main/java/com/basic/myspringboot/auth/userinfo/UserInfo.java
@@ -17,9 +17,11 @@ public class UserInfo {
     @Column(nullable = false)
     private String name;
 
+    //username
     @Column(unique = true, nullable = false)
     private String email;
 
+    //password
     @Column(nullable = false)
     private String password;
 
