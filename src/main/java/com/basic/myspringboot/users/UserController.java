08f9129646dec9ad73677d1ecf01cfe11a974056
mysoyul <vega2k_@naver.com>
2024-05-10 14:20:09 +0900

    templates index.hmtl add

--- src/main/java/com/basic/myspringboot/users/UserController.java
+++ src/main/java/com/basic/myspringboot/users/UserController.java
@@ -1,14 +1,25 @@
 package com.basic.myspringboot.users;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
-        model.addAttribute("name", "basic");
+        model.addAttribute("name", "스프링 부트");
         return "leaf";
     }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "index";
+    }
+
 }
